35eb81dd405f6c39f3fca6741f08a7ed98648583
tomon9086 <tomo.mouse.9086@gmail.com>
2020-10-16 21:45:44 +0900

    add css-loader; support CSS Modules

--- .storybook/main.js
+++ .storybook/main.js
@@ -3,6 +3,13 @@ const path = require("path")
 module.exports = {
   stories: ['../stories/**/*.stories.tsx'],
   webpackFinal: async (config) => {
+    config.module.rules = [
+      ...config.module.rules.filter(rule => rule.test.source !== (/\.css$/).source),
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
     config.resolve.alias = {
       "@": path.resolve(__dirname, "..")
     }
